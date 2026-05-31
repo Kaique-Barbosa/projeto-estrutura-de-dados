2c38de9a2b5bb11ae0f1ca703ee060a499d6b094
Kaique Barbosa <Kaiquebarbosa2103@gmail.com>
2026-05-31 10:19:07 -0300

    criando o meu projeto

---
+++ sistemaDeSenhas.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Aluno: Kaique Almeida Barbosa
+// -----------------------------
+// problematica:  Sistema de atendimento de senhas
+
+
+
+
+
+int main() {
+
+    int opcaoSelecionada, senha, cont =1;
+
+    do {
+        printf("-=-=-=-=-= SIS SENHAS =-=-=-=-=-");
+        printf("1 - gerar Senhas \t");
+        printf("2 - Chamar proxima senha \t");
+        printf("3 - Pesquisar senha \t");
+        printf("4 - Exibir fila de senhas \n");
+        printf("0 - Sair do programa \n");
+        printf("Opcao: ");
+        scanf("%d ", &opcaoSelecionada);
+
+    // SWITCH PARA GERENCIAR AS ENTRADAS ↓↓↓↓4↓↓↓↓↓↓↓↓↓↓ --------------------------------------
+
+    switch (opcaoSelecionada)
+    {
+    case 1 :
+        
+    
+        break;
+    
+    case 2 :
+        
+        break;
+    
+    case 3 :
+        
+        break;
+    
+    case 4 :
+        
+        break;
+    
+    case 0 :
+        
+        break;
+    
+    default:
+        break;
+    }
+
+
+
+
+
+
+
+
+    } while (opcaoSelecionada != 0);
+
+
+    return 0;
+}
